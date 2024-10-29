88e760479b2e474cffe2fe6e25b328f9e6733f23
H1G4O <higor434@hotmail.com>
2024-10-28 21:38:32 -0300

    Cnsultar o saldo

--- main.c
+++ main.c
@@ -1,18 +1,3 @@
-void finalizarCompra(Usuario *usuario, Carrinho *carrinho) {
-    float total = 0;
-    for (int i = 0; i < carrinho->quantidade; i++) {
-        total += carrinho->produtos[i].preco;
-    }
-
-    if (total > usuario->saldo) {
-        printf("Saldo insuficiente para finalizar a compra!\n");
-    } else {
-        usuario->saldo -= total;
-        printf("Compra finalizada! Total: R$%.2f\n", total);
-        carrinho->quantidade = 0; // Limpa o carrinho após a compra
-    }
-}
-
 void verCarteira(Usuario *usuario) {
     printf("Saldo disponível: R$%.2f\n", usuario->saldo);
 }
